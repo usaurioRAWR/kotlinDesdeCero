48b2cc049da3b5c4feca0135e95df0aba7b97e80
usaurio <usaurio@MacBook-Pro-de-usaurio.local>
2021-06-09 04:40:02 +0200

    Inicio

---
+++ Lección 2 - Tipos de datos./com.usauriorawr.usaurioRAWR_Leccion2TiposDeDatos/app/src/main/java/com/usauriorawr/usaurioRAWR_Leccion2TiposDeDatos/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.usauriorawr.usaurioRAWR_Leccion2TiposDeDatos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
