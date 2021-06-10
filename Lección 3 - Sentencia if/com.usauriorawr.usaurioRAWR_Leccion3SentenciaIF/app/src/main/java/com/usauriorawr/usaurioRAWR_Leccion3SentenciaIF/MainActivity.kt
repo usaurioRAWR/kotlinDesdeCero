27d1b5fefa15f823805315bf2f012475257b06ec
usaurio <usaurio@MacBook-Pro-de-usaurio.local>
2021-06-10 16:58:06 +0200

    README

--- Lección 3 - Sentencia if/com.usauriorawr.usaurioRAWR_Leccion3SentenciaIF/app/src/main/java/com/usauriorawr/usaurioRAWR_Leccion3SentenciaIF/MainActivity.kt
+++ Lección 3 - Sentencia if/com.usauriorawr.usaurioRAWR_Leccion3SentenciaIF/app/src/main/java/com/usauriorawr/usaurioRAWR_Leccion3SentenciaIF/MainActivity.kt
@@ -9,3 +9,37 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+// :-----------------------------------------------------------------------------------------------:
+// <Inicio> Aqui vamos a hablar de la sentenci IF.--------------------------------------------------
+// :-----------------------------------------------------------------------------------------------:
+
+private fun sentenciaIF() {
+    var myNumber = 20
+    /*
+        Operadores condicionales
+            > mayor que
+            < menor que
+            >= mayor o igual que
+            <= menor o igual que
+            == igualdad
+            != desigualdad
+
+        Operadores logicos
+            && operador AND (multiplicacion)
+            || operador OR (suma)
+            ! operador NOT (negacion)
+     */
+
+    // Sentencia IF
+    if (myNumber < 20 && myNumber > 10) {
+        println("$myNumber es menor que 20 y mayor que 10")
+    }
+    // Sentencia ELSE
+    else {
+        println("$myNumber es meyor que 20 o menor o igual que 10")
+    }
+}
+// :-----------------------------------------------------------------------------------------------:
+// <Fin> Aqui vamos a hablar de la sentenci IF.-----------------------------------------------------
+// :-----------------------------------------------------------------------------------------------:
+
